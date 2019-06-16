78bf43943f1b45f7cd51372f6f3f0a0486603776
Maros <kiddo@Maross-MacBook-Pro-4.local>
2019-06-16 14:38:09 +0200

    add comments for mqqtClient

--- Assets/Scripts/mqttClient.cs
+++ Assets/Scripts/mqttClient.cs
@@ -27,54 +27,59 @@ public class mqttClient : MonoBehaviour {
     public string mainPlayer = "dwm/node/4da4/uplink/location";
     public string secondPlayer = "dwm/node/9129/uplink/location";
 
+	public string clientIp = "192.168.43.8";
+	public int clientPort = 1883;
+	public string clientUser = "dwmuser";
+	public string clientPass = "dwmpass";
+
     void Start () {
         Player2 = GameObject.Find("Player2");
+
         // create client instance
-
-        // phone instance
-        client = new MqttClient(IPAddress.Parse("192.168.43.8"),1883 , false , null );
-
-        // client = new MqttClient(IPAddress.Parse("192.168.8.100"),1883 , false , null );
-
+        client = new MqttClient(IPAddress.Parse(clientIp),clientPort , false , null );
 
 		// register to message received
 		client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
 
+		// set credential for the mqtt connection
 		string clientId = Guid.NewGuid().ToString();
-		string username = "dwmuser";
-		string password = "dwmpass";
+		string username = clientUser;
+		string password = clientPass;
 		client.Connect(clientId, username, password);
 
-		// subscribe to the topic "/home/temperature" with QoS 2
+		// subscribe to the topics with QoS 2
 		client.Subscribe(new string[] { mainPlayer, secondPlayer }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
 
 	}
 	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
 	{
-        string toping = e.Topic;
+        string topic = e.Topic;
         string message = System.Text.Encoding.UTF8.GetString(e.Message);
         string json = JsonUtility.ToJson(message);
         jsonNode = SimpleJSON.JSON.Parse(message);
 
-        if (toping == mainPlayer)
+        if (topic == mainPlayer)
         {
             x = float.Parse(jsonNode["position"]["x"]);
             y = float.Parse(jsonNode["position"]["y"]);
             z = float.Parse(jsonNode["position"]["z"]);
         }
-        if (toping == secondPlayer)
+        if (topic == secondPlayer)
         {
             x2 = float.Parse(jsonNode["position"]["x"]);
             y2 = float.Parse(jsonNode["position"]["y"]);
             z2 = float.Parse(jsonNode["position"]["z"]);
         }
     }
+	// transform players position based on incoming data
 	void transformPlayer() {
+		// player 1 transform position
 		if (!float.IsNaN(x) && !float.IsNaN(y) && !float.IsNaN(z))
 		{
 			playerPosition = new Vector3(x / division, z/division, y/division);
 			transform.position = playerPosition;
 		}
+		// player 2 transform position
         if (!float.IsNaN(x2) && !float.IsNaN(y2) && !float.IsNaN(z2))
         {
             player2Position = new Vector3(x2 / division, z2 / division, y2 / division);
